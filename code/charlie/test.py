4cb16f6836e156b7a18ddfdaebf85a3e94b65be8
Charles Spahn <charlies87@gmail.com>
2021-01-26 15:58:42 -0800

    update

---
+++ code/charlie/test.py
@@ -0,0 +1,9 @@
+s = "You gotta lie to kick it"
+l = s.split(" ")
+l2 = ["P", "M", "C"]
+s2 = ", ".join(l2)
+
+print(f"This is the split string: {l}")
+print(f"This is the conjoined string: {s2}")
+
+print("This is what a delta looks like: \u0394")
