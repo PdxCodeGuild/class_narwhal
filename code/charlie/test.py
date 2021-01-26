c4ee0fb727b9bd7c97d83df5affecdede686c78d
Charles Spahn <charlies87@gmail.com>
2021-01-26 15:05:01 -0800

    Deleted test file

--- code/charlie/test.py
+++
@@ -1,9 +0,0 @@
-s = "You gotta lie to kick it"
-l = s.split(" ")
-l2 = ["Pussy", "Money", "Weed"]
-s2 = ", ".join(l2)
-
-print(f"This is the split string: {l}")
-print(f"This is the conjoined string: {s2}")
-
-print("This is what a delta looks like: \u0394")
