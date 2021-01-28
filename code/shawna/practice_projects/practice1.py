3aad8fb7e020105a063bfc2646d33d6591e44b04
Shawna Duvall <shawnabduvall@gmail.com>
2021-01-27 22:16:34 -0800

    practice 1.1 complete

---
+++ code/shawna/practice_projects/practice1.py
@@ -0,0 +1,13 @@
+# Instructions: For each practice problem, write a function that returns a value (not just prints it). 
+# You can then call the function a couple times to test it, comment those calls out, 
+# and move on to the next problem.
+
+#-------1.1-------
+# Write a function that tells whether a number is even or odd (hint, compare a/2 and a//2, or use a%2)
+
+def is_even(num):
+   return True if num % 2 == 0 else False
+
+print(is_even(9))
+
+#-------1.2-------
