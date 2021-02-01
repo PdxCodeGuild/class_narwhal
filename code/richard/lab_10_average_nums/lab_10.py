30bd9e8da21593abbf5fc5832aeec12ff6ae46ef
Richard Farr <RichardFarr7@gmail.com>
2021-02-01 15:31:54 -0800

    input validations with floats

--- code/richard/lab_10_average_nums/lab_10.py
+++ code/richard/lab_10_average_nums/lab_10.py
@@ -51,15 +51,16 @@ user_input = 0
 
 while True:
     user_input = input("enter a number, or 'done': ")
+
     if user_input == "done" or user_input == " ":
         break
-    
+
     try:
-        val = int(user_input)
+        val = float(user_input)
         num_list.append(float(user_input))
         continue
     except ValueError:
-        print("That's not an int!")
+        print("That's not a number!")
 
         
 
