0817dd00c6ea067e4993654e92c8cb8154c5ee37
Mike <mrprice77@gmail.com>
2021-02-02 10:05:51 -0800

    Jackalope Simulator

---
+++ code/mike/jackalope.py
@@ -0,0 +1,18 @@
+'''
+Version 1
+The goal is to calculate how many years it will take for two jackalopes to create a population of 1000.
+
+Jackalopes are reproductive from ages 4-8 and die at age 10.
+Gestation is instantaneous. Each gestation produces two offspring.
+Jackalopes are hermaphrodites, it takes a pair to reproduce, but any pair will do
+With these conditions in mind, we can represent our population as a list of ints.
+'''
+
+    
+
+def 
+
+
+
+
+for n in range(1000):
