26ce58287de106d3920116f6b2c3cfd1e8929acd
Charles Spahn <charlies87@gmail.com>
2021-02-01 21:54:35 -0800

    Update

---
+++ code/charlie/rot13.py
@@ -0,0 +1,22 @@
+# def encode(to_encode, alpha):
+#     for i in range(len(to_encode)):
+#         #if to_encode[i] == alpha
+
+
+
+#     return 0
+        
+        
+
+
+def main():
+
+    alpha = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+    # temp = input("Please enter a string of characters, it can be a word or completely random letters, and this program will use a cipher to encode/decode it:")
+    # to_encode =[char for char in temp]
+    # encrypted = encode(to_encode, alpha)
+    w = ['f', 'w']
+    i = alpha.index(w[0])
+    print(i)
+
+main()
