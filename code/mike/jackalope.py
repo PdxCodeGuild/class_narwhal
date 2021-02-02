c5645833a8fd9c9c05f2c3a1dfba66badc208c88
Charles Spahn <charlies87@gmail.com>
2021-02-02 10:32:56 -0800

    update

--- code/mike/jackalope.py
+++ code/mike/jackalope.py
@@ -10,9 +10,22 @@ With these conditions in mind, we can represent our population as a list of ints
 
     
 
-def 
 
 
+def main():
+
+    age_of_jacks = [0, 0]
+    num_of_jacks = 2
+    
+    while num_of_jacks <= 1000:
+
+        for i in range(len(age_of_jacks)):
+            
+        if 4 =< age_of_jacks[] <= 8:
+            num_of_jacks += 2
 
 
-for n in range(1000):
+   # for n in range(1000):
+
+
+main()
