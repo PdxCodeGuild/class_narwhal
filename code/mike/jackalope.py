bff2422ddfc6aea7aff39d72b3eaa46954e9fd81
Mike <mrprice77@gmail.com>
2021-02-02 11:52:45 -0800

    updated jackalope

--- code/mike/jackalope.py
+++ code/mike/jackalope.py
@@ -7,29 +7,50 @@ Gestation is instantaneous. Each gestation produces two offspring.
 Jackalopes are hermaphrodites, it takes a pair to reproduce, but any pair will do
 With these conditions in mind, we can represent our population as a list of ints.
 '''
-population = 2
+
+
+
+def check_age(age_of_jacks):
+    x = []
+    for i in range(len(age_of_jacks)):
+        if 4 <= age_of_jacks[i] <= 8:
+            age_of_jacks.append(0)
+            age_of_jacks[i] += 1
+        x = age_of_jacks
+    # return age_of_jacks
+
+
+
+def main():
+
+
 years = 0
-# Life of a jackalope
+age_of_jacks = [5, 6]
 
-max_age = 10
-current_age = 0
 
-while population <= 1000:
-    while current_age < max_age:
-        if  4 <= current_age <= 8:
-            population += 2
-            current_age += 1
-            years += 1
-        elif current_age == 10:
-            population -= 2
-            current_age += 1
-            years += 1
-        else:
-            current_age += 1
-            years += 1
+while len(age_of_jacks) <= 1000:
+    age_of_jacks = check_age(age_of_jacks)
 
-print(population)
-print(years)
+
+print(age_of_jacks)
+
+main()
+# while population <= 1000:
+#     while current_age < max_age:
+#         if  4 <= current_age <= 8:
+#             population += 2
+#             current_age += 1
+#             years += 1
+#         elif current_age == 10:
+#             population -= 2
+#             current_age += 1
+#             years += 1
+#         else:
+#             current_age += 1
+#             years += 1
+
+# print(population)
+# print(years)
 
  #if age is between 4 and 8 add 2 offspring per year
 
@@ -43,7 +64,7 @@ print(years)
 
 # def main():
 
-#     age_of_jacks = [0, 0]
+#     
 #     num_of_jacks = 2
     
 #     while num_of_jacks <= 1000:
