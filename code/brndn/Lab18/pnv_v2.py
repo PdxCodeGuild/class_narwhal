88a6404846c3e4dfd3b34148468e724cadd5bdff
Brandon Rimes <chimaybidet@The-Matrix.local>
2021-02-04 18:53:01 -0500

    added Lab18 V2

--- code/brndn/Lab18/pnv_v2.py
+++ code/brndn/Lab18/pnv_v2.py
@@ -6,5 +6,18 @@
 
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
-for i in data:
-    
+x = 9
+
+print('\n')
+
+while x > 0:
+    line = ''
+    for i in data:
+        if i < x:
+            line += '  '
+        elif i >= x:
+            line += '\N{THUMBS UP SIGN}'
+    print(line)
+    x -= 1
+
+print('\n')
