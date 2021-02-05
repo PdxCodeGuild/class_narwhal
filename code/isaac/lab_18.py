539982f4fb79b1dae8ec8281a598ab8c7ea6f448
ifm1986 <mitchellisaac23@yahoo.com>
2021-02-05 11:00:53 -0800

    Version 1

---
+++ code/isaac/lab_18.py
@@ -0,0 +1,37 @@
+
+# The Peak and the Valleys
+
+data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
+
+def peaks(data):
+    peaks = []
+    print(data)
+    #return data
+    
+    for x in range(len(data)):
+        #print(data[x])
+        if data[x] == 1:
+            pass
+           #print(True)
+
+            if range(data[x])== 0:
+                print(data[x])
+    
+
+
+peaks(data)
+
+
+
+
+
+
+
+#def valleys(data):
+     
+
+
+
+
+
+#print(data)
