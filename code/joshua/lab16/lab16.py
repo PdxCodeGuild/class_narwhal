002bfb514146c3910a5aa7a1ec2b608a48f5f327
novacj <77870451+novacj@users.noreply.github.com>
2021-02-05 14:18:25 -0800

    added lab16.py

--- code/joshua/lab16/lab16.py
+++ code/joshua/lab16/lab16.py
@@ -51,7 +51,7 @@ Colorsys represents colors as floats in the range 0.0 - 1.0, whereas pillow uses
 You'll have to convert between these two representations.
 '''
 
-
+'''
 import colorsys
 
 from PIL import Image
@@ -83,18 +83,17 @@ for i in range(width):
         pixels[i, j] = (r, g, b)
 
 img.show()
-
-
-
-
 '''
-## Version 3
 
+
+## Version 3
+'''
 Pillow can also be used to draw, the code below demonstrates some functions that Pillow provides. 
 Use these functions to draw a stick figure.
 You can find more documentation [here](http://pillow.readthedocs.io/en/4.2.x/reference/ImageDraw.html).
+'''
 
-```python
+
 from PIL import Image, ImageDraw
 
 width = 500
@@ -106,27 +105,35 @@ draw = ImageDraw.Draw(img)
 
 
 # the origin (0, 0) is at the top-left corner
-
+## background
 draw.rectangle(((0, 0), (width, height)), fill="white")
 
 # draw a rectangle from x0, y0 to x1, y1
-draw.rectangle(((100, 100), (300, 300)), fill="lightblue")
+## arms
+draw.rectangle(((50, 200), (450, 225)), fill="pink")
+## left leg
+draw.rectangle(((150, 300), (200, 500)), fill="pink")
+## right leg
+draw.rectangle(((300, 300), (350, 500)), fill="pink")
+## body
+draw.rectangle(((150, 150), (350, 350)), fill="lightblue")
 
 # draw a line from x0, y0, x1, y1
 # using the color pink
+'''
 color = (256, 128, 128)  # pink
 draw.line((0, 0, width, height), fill=color)
 draw.line((0, height, width, 0), fill=color)
-
+'''
 
 circle_x = width/2
-circle_y = height/2
-circle_radius = 100
+circle_y = height/5
+circle_radius = 60
 draw.ellipse((circle_x-circle_radius, circle_y-circle_radius, circle_x+circle_radius, circle_y+circle_radius), fill='lightgreen')
 
 img.show()
-```
 
+'''
 Try running the code below, which generates 1000 random lines with random colors.
 
 ```python
