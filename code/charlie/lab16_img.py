63b5b05f324a70ce49cd3a4d50f17622e61cee5e
Brandon Rimes <chimaybidet@The-Matrix.local>
2021-02-05 14:40:42 -0500

    lab16 v1 completed

--- code/charlie/lab16_img.py
+++ code/charlie/lab16_img.py
@@ -1,18 +1,21 @@
 from PIL import Image
-img = Image.open("F_Douglass.jpg") # must be in same folder
+img = Image.open("F_Douglass.jpeg") # must be in same folder
 width, height = img.size
 pixels = img.load()
 
 for i in range(width):
     for j in range(height):
         r, g, b = pixels[i, j]
-        r *= 0.299
-        g *= 0.587
-        b *= 0.114 
+
+        y = 0.299*r + 0.587*g + 0.114*b
+
+        r = y
+        g = y
+        b=y
+        
         r = int(r)
         g = int(g)
         b = int(b)
-        #Y = 0.299*R + 0.587*G + 0.114*B
         pixels[i, j] = (r, g, b)
 
 img.show()
