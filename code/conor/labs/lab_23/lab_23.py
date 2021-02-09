21ab2b8ac17d5138e1e5c3a9ccff5f654f1ee4ac
Conor Dondale <77942122+dondco36@users.noreply.github.com>
2021-02-09 11:05:12 -0800

    Lab 23 Working ish

--- code/conor/labs/lab_23/lab_23.py
+++ code/conor/labs/lab_23/lab_23.py
@@ -16,7 +16,7 @@ with open('contacts.csv') as file:
         contact_list.append(contact)
 
 
-print(contact_list)
+# print(contact_list)
 
 # - Version 2 - #
 # - Create, Read, Update and Delete Functions - #
@@ -31,27 +31,57 @@ def create(contact_list, header):
 # print(contact_list)
 
 def read(contact_list):
-    read = input('What is the name of the contact you would like to see? ')
+    read = input('Please enter the name of the contact? ')
     for contact in contact_list:
         c = contact
         if c.get("name") == read:
-            return c
+            print(c)
 
-# print(read(contact_list, header))
+# read(contact_list, header)
 
 def update(contact_list):
     update = read(contact_list)
     contact_to_update = input("Please enter the category you would like to update: ")
     update_contact = input(f"What would you like to change it to?  ")
     update[contact_to_update] = update_contact
-    return update
+    return update_contact
 
-# print(update(contact_list))
+# update(contact_list)
 
-def delete():
-    
+def delete(contact_list):
+    delete = input('Which contact would you like to delete? ')
+    for i, contact in enumerate(contact_list):
+        if contact == delete:
+            del contact_list[i]
+
+# delete(contact_list)
+
+# print(contact_list)
 
 # - Implement a CRUD REPL - #
+
+# create = create(contact_list, header)
+# read = read(contact_list)
+# update = update(contact_list)
+# delete = delete(contact_list)
+
+# while True:
+#     print('Welcome to your Contacts List!')
+#     print("Enter 'quit' if you are finished")
+#     task = input("Would you lke to 'create', 'read', 'update', or 'delete' a contact? ")
+#     if task == 'quit':
+#         print('Have a good day!')
+#         break
+#     elif task == 'create':
+#         create(contact_list, header)
+#     elif task == 'read':
+#         read(contact_list)
+#     elif task == 'update':
+#         update(contact_list)
+#     elif task == 'delete':
+#         delete(contact_list)
+
+# print(contact_list)
 
 # - Version 3 - #
 # - Save the updated Contact List CSV - #
