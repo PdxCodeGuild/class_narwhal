570f1eb8f7da1ea3c0167c02b7005808ac8ddefd
Richard Farr <RichardFarr7@gmail.com>
2021-02-08 16:14:47 -0800

    lab 23 - not finished

--- code/richard/lab_23_contact_list/lab_23_contacts.py
+++ code/richard/lab_23_contact_list/lab_23_contacts.py
@@ -48,32 +48,70 @@ contacts.append(user_input)
 print(contacts)
 '''
 
-## Retieve
+## Retrieve
 
 # 1. Ask the user what contact to retrieve
-retieve_name = input("What person would you like the info on? ")
+retrieve_name = input("What person would you like the info on? ")
 
 
 
 # 2. Spit out that output (if it exists)
-for item in range(len(contacts)):
-    # print(contacts[item].values())
-    print(contacts[item].get(retieve_name)) # issues here
-    '''
-    if retieve_name in contacts[item].keys(): #[name].keys()
-        print(contacts[item].get(item))
-    else:
-        print("nobody by that name")
-        break
-'''
+retrieved_contact = None
+for contact in contacts:
+    if contact["name"] == retrieve_name:
+        retrieved_contact = contact
+        print(contact)
+if retrieved_contact == None:
+    print("Nobody by that name")
+
 
 
 ## Update
 
+update_question = input("Would you like to update any field on the contact we just recieved? (Y/N) ")
+update_question = update_question.lower()
+
+if update_question == "y" or update_question == "yes":
+    print("OK, lets update the record")
+    update_field = input("What field would you like to update? (name, favorite_fruit, favorite_color, favorite_state) ")
+    update_field = update_field.lower()
+    update_value = input("What would you like to update it to? ")
+    print(f"OK, lets update {update_field} to {update_value}")
+    contact.update({update_field: update_value})
+    print("heres the new contact info")
+    print(contact)
+    # if update_field == name:
+        #retrieved_contact.update({"name": update_value })
+print(f"New information is: ")
+print(contacts)
+
+
 ## Delete
+delete_question = input("Would you like to delete a record? (Y/N) ")
+delete_question = delete_question.lower()
+
+if delete_question == "y" or update_question == "yes":
+    print("OK, lets delete a record")
+    delete_item = int(input("Whats the position of the record you would like to delete? "))
+    del contacts[delete_item]
+    print("Here are the contacts after we deleted that one: ")
+    print(contacts)
+
+
+    
+
+    
+# print(f"New information is: {contact}")
+
+
+
+
 
 
 # Version 3 - Save everything back to the contacts.csv file
+
+
+# The Easy way - use a python module
 import csv
 
 with open("contacts_updated.csv", "w", newline="") as csv_file:
