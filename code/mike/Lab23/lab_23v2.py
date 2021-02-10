c18db791fdced0ad87dde82d2eff9919583ff7e1
Mike Price <MrPrice77@gmail.com>
2021-02-09 16:35:33 -0800

    Updated work on Lab 23 Version 2

--- code/mike/Lab23/lab_23v2.py
+++ code/mike/Lab23/lab_23v2.py
@@ -1,28 +1,82 @@
 with open('contacts.csv', 'r') as file: # Opens contacts.csv
-    lines = file.read().split('\n') # reads lines in file and splits on line break
-rows = lines # identifies lines as rows
-header = rows[0].split(',') # Identifies the top row as header
-
+    rows = file.read() # reads file as rows
 contacts = [] # creates empty list
 
-for x in range(1, len(rows)): # starts with first row after header
-    row = rows[x].split(',') # identifies singular row
-    contact = dict(zip(header, row)) # creates dictionary called contact zipping the header and row
-    contacts.append(row) # adds the contract to the contracts list
+rows = rows.split('\n') # separates rows with line break
+header = rows[0].split(',') # Identifies the top row as header
 
+# starts list of contacts on the first row after header
+for x in range(1, len(rows)):
+    row = rows[x].split(',')
+    contact = dict(zip(header, row))
+    contacts.append(contact)
 
-# Create new contact
-new_contact = {} # empty dictionary for contact
-for info in header: 
-    details = input(f'Enter contact\'s {info}: ') # user inputs the contact information based on the header value
-    new_contact[info] = details
+# create contact
+def contact_info():
+    new_contact = {} # empty dictionary for contact
+    for info in header: 
+        details = input(f'Enter contact\'s {info}: ') # user inputs the contact information based on the header value
+        new_contact[info] = details
     contacts.append(new_contact) # after loop, adds the contact list to the list of dictionaries
 
 
-print(contacts)
+# update contact
+def contact_update():
+    contact = input('Which contact would you like to update? ')
+    category = input(f'Which category do you want to update? {header}: ')
+    ind = header.index(category)
+    update_info = input(f'What do you want to change {category} to? ')
+    for info in contacts:
+        if contact == info['name']:
+            contact = info['name'], info['favorite fruit'], info['favorite color']
+            contact = list(contact)
+            contact[ind] = update_info
+            contact = tuple(contact)
+            contact = ' '.join(contact)
+            print(contact)
 
+
+
+# looks up contact
+def contact_lookup():
+    for category in header:
+        category = input(f'What category do you want to search by? {header}: ')
+        data = input(f'Enter contact\'s {category}: ')
+        for info in contacts:
+            if data == info[category]:
+                data = info['name'], info['favorite fruit'], info['favorite color']
+                data = ' '.join(data)
+                return data
+    
+# loop to determine user action
+while True:
+    action = input('What would you like to do? (new/search/update): ')
+
+    # create new contact
+    if action == 'new':
+        contact_info()
+        break
+
+    # search for contact by name
+    elif action == 'search':
+        print(contact_lookup())
+        break
+    
+    elif action == 'update':
+        contact_update()
+        break
+        
+
+    # break's loop if "new" or "search" not selected
+    else:
+        break
+
+
+# print(contacts)
+
+# writes new contacts into csv file
 rows = [','.join(header)]
-for contact in contacts:
+for new_contact in contacts:
     row = ','.join(new_contact.values())
     rows.append(row)
 write_contacts = '\n'.join(rows)
