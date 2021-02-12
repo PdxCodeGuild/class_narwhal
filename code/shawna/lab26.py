28a70734697b5955f3998d080c25e164df05244a
Shawna Duvall <shawnabduvall@gmail.com>
2021-02-12 11:19:11 -0800

    lab 26

--- code/shawna/lab26.py
+++ code/shawna/lab26.py
@@ -1,18 +1,38 @@
+
+class Player:
+
+    def __init__(self, name, token):
+        self.name = name
+        self.token = token
+
 class GameBoard:
  
     def __init__(self):
         self.board = {
         #(x,y)
-        (0,0) : "O",
+        (0,0) : " ",
         (0,1) : " ",
         (0,2) : " ",
         (1,0) : " ",
-        (1,1) : "O",
+        (1,1) : " ",
         (1,2) : " ",
         (2,0) : " ",
         (2,1) : " ",
         (2,2) : " ",
         }
+
+        # self.board = {
+        # #(x,y)
+        # (0,0) : "X",
+        # (0,1) : "X",
+        # (0,2) : "X",
+        # (1,0) : "X",
+        # (1,1) : "X",
+        # (1,2) : "X",
+        # (2,0) : "X",
+        # (2,1) : "X",
+        # (2,2) : "X",
+        # }
 
     def __repr__(self):
         #x is horizontal, y is vertical positions are(x,y)
@@ -37,21 +57,36 @@ class GameBoard:
         #check column
         if self.board[(x,0)] == player.token and self.board[(x,1)] == player.token and self.board[(x,2)] == player.token:
             print(f"{player.name} is the winner.")
+            return True
         #check row
         elif self.board[(0,y)] == player.token and self.board[(1,y)] == player.token and self.board[(2,y)] == player.token:
             print(f"{player.name} is the winner.")
+            return True
         #check diagonal 
         elif self.board[(0,0)] == player.token and self.board[(1,1)] == player.token and self.board[(2,2)] == player.token:
             print(f"{player.name} is the winner.")
+            return True
         #continue
         else:
             return False
 
-class Player:
+    def is_full(self):
+        boxes = [[i for i in self.board[x]] for x in self.board.keys()] 
+        for box in boxes:
+            if box[0] == " ":
+                return False
+        return True
 
-    def __init__(self, name, token):
-        self.name = name
-        self.token = token
+    def is_over(winner, full):
+
+        if winner ==True:
+            return True
+        elif full == True:
+            print("Board is full and no one won.")
+            return True
+        else:
+            return False
+
 
 
 #------------------------------------------------------------------------------------#
@@ -62,7 +97,7 @@ print("Let's Play Tic-Tac-Toe")
 
 while True:
 
-    #gather player one info
+    #player one info
     p1_name = input("Player 1, enter your name: ")
     p1_token = ""
     while not (p1_token == 'X' or p1_token == 'O'):
@@ -71,30 +106,41 @@ while True:
     #Create player one
     p1 = Player(p1_name, p1_token)
 
-    #gather player two info
+    #player two info
     p2_name = input("Player 2, enter your name: ")
     p2_token = ""
     while not (p2_token == 'X' or p2_token == 'O'):
         p2_token = (input("Would you like to be X's or O's: ")[0]).upper()
     
-    #Create player one
+    #Create player two
     p2 = Player(p2_name, p2_token)
 
     #Print the empty board
     board = GameBoard()
     print(board.__repr__())
-    
-    #player 1 move
-    print(f"{p1.name} it is your turn.")
-    x = ""
-    while not (x == "1" or x == "2" or x == "3"):
-        x = input("Which COLUMN would you like to place your token? 1, 2, or 3?\nColumn: ")
-    y = ""
-    while not (y == "1" or y == "2" or y == "3"):
-        y = input("Which ROW would you like to place your token? 1, 2, or 3?\nRow: ")
-    
-    board.move(x,y,p1)
-    board.calc_winner(x,y,p1)
+    current_player = ""
+    while True:
+        if current_player == p1: 
+            current_player = p2 
+        else: 
+            current_player = p1
+        #player 1 move
+        print(f"{current_player.name} it is your turn.")
+        x = ""
+        while not (x == "1" or x == "2" or x == "3"):
+            x = input("Which COLUMN would you like to place your token? 1, 2, or 3?\nColumn: ")
+        y = ""
+        while not (y == "1" or y == "2" or y == "3"):
+            y = input("Which ROW would you like to place your token? 1, 2, or 3?\nRow: ")
+        
+        board.move(x,y,current_player)
+        print(board.__repr__())
 
-    print(board.__repr__())
-
+        winner = (board.calc_winner(x,y,current_player))
+        if board.is_over(winner) == True:
+            break
+        
+    again = input("Play again? Y/N: ")
+    if again.upper() == "N":
+        break
+        
