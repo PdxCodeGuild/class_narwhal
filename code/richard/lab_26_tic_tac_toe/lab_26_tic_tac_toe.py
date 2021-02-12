7758380e8ae500f0f36ea76d9646e64704931497
Richard Farr <RichardFarr7@gmail.com>
2021-02-12 08:17:53 -0800

    lab 26 - minimal progress

--- code/richard/lab_26_tic_tac_toe/lab_26_tic_tac_toe.py
+++ code/richard/lab_26_tic_tac_toe/lab_26_tic_tac_toe.py
@@ -20,74 +20,92 @@ class Player:
         self.name = name # these are member variables
         self.token = token
 
+player_1 = Player("Richard", "X")
+player_2 = Player("Theresa", "O")
+
 
 '''
 The Game class has the following properties:
 
 board = your representation of the board
-
 You can represent the board however you like, such as a 2D list, tuples, or dictionary.
+
 '''
+
+
+def drawBoard():
+    board = ["","","","","","","","",""]
+    print(board[0] + '|' + board[1] + '|' + board[2] )
+    print('-+-+-')
+    print(board[3] + '|' + board[4] + '|' + board[5] )
+    print('-+-+-')
+    print(board[6] + '|' + board[7] + '|' + board[8] )
+
+drawBoard()
+
+
 
 class Game:
     def __init__(self, board):
         self.board = board
 
+    # def drawBoard(self):
 
 
 
 '''
 The Game class has the following methods:
+
  __repr__()` Returns a pretty string representation of the game board
 
->>> print(board)
+print(board)
 X| | 
 O|X|O
  | | 
-```
+
 
 * `move(x, y, player)` Place a player's token character string at a given coordinate (top-left is 0, 0), 
 x is horizontal position, y is vertical position.
 
-```py
+py
 >>> board.move(2, 1, player_1)
  | | 
  | |X
  | | 
-```
+
 
 * `calc_winner()` What token character string has won or `None` if no one has.
 
-```py
+
 X| | 
 O|X|O
  | |X
->>> board.calc_winner()
+board.calc_winner()
 X
-```
+
 
 * `is_full()` Returns true if the game board is full.
 
-```py
+
 X|O|X
 X|X|O
 O|O|X
->>> board.is_full()
+board.is_full()
 True
-```
+
 
 * `is_game_over()` Returns true if the game board is full or a player has won.
 
-```py
+
 X|O|X
 X|X|O
 O|O|X
->>> board.is_game_over()
+board.is_game_over()
 True
 
 X|O|
  | |X
  | |
->>> board.is_game_over()
+board.is_game_over()
 False
 '''
---
+++ code/richard/lab_26_tic_tac_toe/non_ooj_way.py
@@ -0,0 +1,12 @@
+# Tic Tac Toe
+# Richard Farr 
+# Adapted from a book I bought a few years back and did not read
+# Non Object Oriented Approach
+# Will use later to help do the same thing but in an OOJ approach
+
+import random
+
+def drawBoard(board):
+    # Print out a board
+    print("x")
+
