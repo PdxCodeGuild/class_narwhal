a7a8796064f015b1b7243765ee1c189ebc65c7a2
novacj <77870451+novacj@users.noreply.github.com>
2021-02-12 19:02:32 -0800

    added finished lab26.py

--- code/joshua/lab26/lab26.py
+++ code/joshua/lab26/lab26.py
@@ -6,136 +6,176 @@ PDX Code Guild
 '''
 
 
-'''///////////// CLASSES /////////////'''
+'''//////////////////////// CLASSES/FUNCTIONS ////////////////////////'''
 class Player:
     def __init__(self, name, token):
         self.name = name
         self.token = token
 
+    def __str__(self):
+        return self.token
+
 class Game:
     def __init__(self):
-        self.board = [' ',' ',' '
-                     ,' ',' ',' '
-                     ,' ',' ',' ']
+        self.board = [ ' ',' ',' ',
+                       ' ',' ',' ',
+                       ' ',' ',' ' ]
 
     def __repr__(self):
         board = self.board
-        return f'{board[0]} | {board[1]} | {board[2]}\n{board[3]} | {board[4]} |{board[5]}\n{board[6]} | {board[7]} | {board[8]}'
-    
-         ## Work in progress, may change ##
+        return f'{board[0]}|{board[1]}|{board[2]}\n{board[3]}|{board[4]}|{board[5]}\n{board[6]}|{board[7]}|{board[8]}'
 
-        #example - board.move(x, y, player)
-    '''    
-    def move(self, x, y, player):
-        if x == 0 and y == 0:
-            i = '0'
-        elif x == 1 and y == 1:
-            i = '1'
-        elif x == 2 and y == 2:
-            i = '2'
-        elif x == 3 and y == 3:
-            i = '3'
-        elif x == 4 and y == 4: 
-            i = '4'
-        elif x == 5 and y == 5:
-            i = '5'
-        elif x == 6 and y == 6:
-            i = '6'
-        elif x == 7 and y == 7:
-            i = '7'
-        elif x == 8 and y == 8: 
-            i = '8'
-        else:
+    def move(self, position, player):
+        if self.board[position - 1] != ' ':
             return False
-        '''
-
-    def calc_winner(self):
-        #### VERTICAL WINS #### (x = horizontal position; y = veritcal position)
-        ''' X|-|-  
-            X|-|- 
-            X|-|- '''
-        if self.board[0] == 'x' and self.board[3] == 'x' and self.board[6] == 'x':
+        else:
+            self.board[position - 1] = f'{player.token}'
             return True
-        if self.board[0] == 'y' and self.board[3] == 'y' and self.board[6] == 'x':
-            return True
-        ''' -|X|- 
-            -|X|- 
-            -|X|- '''
-        if self.board[1] == 'x' and self.board[4] == 'x' and self.board[7] == 'x':
-            return True
-        if self.board[1] == 'y' and self.board[4] == 'y' and self.board[7] == 'y':
-            return True
-        ''' -|-|X 
-            -|-|X 
-            -|-|X '''
-        if self.board[2] == 'x' and self.board[5] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[2] == 'y' and self.board[5] == 'y' and self.board[8] == 'y':
-            return True
-
-        #### HORIZONTAL WINS #### (x = horizontal position; y = veritcal position)
-        ''' X|X|X 
-            -|-|- 
-            -|-|- '''
-        if self.board[0] == 'x' and self.board[1] =='x' and self.board[2] == 'x':
-            return True
-        if self.board[0] == 'y' and self.board[1] == 'y' and self.board[2] == 'y':
-            return True
-        ''' -|-|- 
-            X|X|X
-            -|-|- '''
-        if self.board[3] == 'x' and self.board[4] == 'x' and self.board[5] == 'x':
-            return True
-        if self.board[3] == 'y' and self.board[4] == 'y' and self.board[5] == 'y':
-            return True
-        ''' -|-|- 
-            -|-|- 
-            X|X|X '''
-        if self.board[6] == 'x' and self.board[7] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[6] == 'y' and self.board[7] == 'y' and self.board[8] == 'y':
-            return True
-
-        #### DIAGONAL WINS #### (x = horizontal position; y = veritcal position)
-        ''' X|-|- 
-            -|X|- 
-            -|-|X '''
-        if self.board[0] == 'x' and self.board[4] == 'x' and self.board[8] == 'x':
-            return True
-        if self.board[0] == 'y' and self.board[4] == 'y' and self.board[8] == 'y':
-            return True
-        ''' -|-|X 
-            -|X|- 
-            X|-|- '''
-        if self.board[6] == 'x' and self.board[4] == 'x' and self.board[2] == 'x':
-            return True
-        if self.board[6] == 'y' and self.board[4] == 'y' and self.board[2] == 'y':
-            return True
+        
+    def calc_winner(self, player):
+        #### ALL VERTICAL WINS ####
+        #  X|-|-
+        #  X|-|-
+        #  X|-|-
+        if self.board[0] == self.board[3] == self.board[6] == player.token:
+            return player.token
+        #  -|X|-
+        #  -|X|-
+        #  -|X|-
+        if self.board[1] == self.board[4] ==  self.board[7] == player.token:
+            return player.token
+        #  -|-|X
+        #  -|-|X
+        #  -|-|X
+        if self.board[2] == self.board[5] == self.board[8] == player.token:
+            return player.token
+        #### ALL HORIZONTAL WINS ####
+        #  X|X|X
+        #  -|-|-
+        #  -|-|-
+        if self.board[0] == self.board[1] == self.board[2] == player.token:
+            return player.token
+        #  -|-|-
+        #  X|X|X
+        #  -|-|-
+        if self.board[3] == self.board[4] == self.board[5] == player.token:
+            return player.token
+        #  -|-|-
+        #  -|-|-
+        #  X|X|X
+        if self.board[6] == self.board[7] == self.board[8] == player.token:
+            return player.token
+        #### ALL DIAGONAL WINS ####
+        #  X|-|-
+        #  -|X|-
+        #  -|-|X
+        if self.board[0] == self.board[4] == self.board[8] == player.token:
+            return player.token
+        #  -|-|X
+        #  -|X|-
+        #  X|-|-
+        if self.board[6] == self.board[4] == self.board[2] == player.token:
+            return player.token
+        return False
 
     def is_full(self):
         for i in self.board:
             if ' ' not in self.board:
                 return True
 
-    def is_game_over(self):
-        if self.calc_winner() == True:
-            print('Game Over!')
-            print(f'Thank you for playing Python Tic-Tac-Toe!')
-            quit()
-        elif self.is_full() == True:
-            print('Tie Game!')
-            print(f'Thank you for playing Python Tic-Tac-Toe!')
-            quit()
+    def is_game_over(self, player):
+        if self.calc_winner(player) == player.token:
+            return True
+        else:
+            if self.is_full() == True:
+                return True
+            else:
+                return False
 
-'''////////////////REPL////////////////'''
+# prints the game rules for players that type 'help'
+def help():
+    print('\n----------------------------------------\n\t\t RULES\n----------------------------------------\n'\
+    '\t  Enter a number 1-9\n1 = TOP LEFT, 2 = TOP MID, 3 = TOP RIGHT'\
+    '\n4 = MID LEFT, 5 = MID MID, 6 = MID RIGHT\n7 = BOT LEFT, 8 = BOT MID, 9 = BOT RIGHT'\
+    '\n----------------------------------------\n')
+
+
+'''///////////////////////////////////////////////////////////REPL CODE///////////////////////////////////////////////////////////'''
 def main():
 
-    print('\nPython Tic-Tac-Toe\n')
-    player_1 = Player(input('/ PLAYER ONE / - ENTER NAME: ').title(), 'X')
-    player_2 = Player(input('// PLAYER TWO // - ENTER NAME: ').title(), 'O')
-    print(f'\n{player_1.name}: You are X\'s')
-    print(f'{player_2.name}: You are O\'s\n')
+    # welcome message to players
+    print('\n    //////PYTHON TIC-TAC-TOE//////\n')
+    # enter players names
+    player_1 = Player(input('/PLAYER 1 NAME/: ').title(), 'X')
+    player_2 = Player(input('//PLAYER 2 NAME//: ').title(), 'O')
+    # program assigns tokens to players
+    print(f'\n\n{player_1.name}: X\'s\t\t\t{player_2.name}: O\'s\n')
+    # X's go first
+    print(f'\t   {player_1.name} will go first!\n')
+    print(f'xx      xx      o ooo o      xx      xx\n  xx  xx      oo       oo      xx  xx\n    xx       oo         oo       xx'\
+        '\n  xx  xx      oo       oo      xx  xx\nxx      xx      o ooo o      xx      xx')
+    # available move sequences for players
+    print('\n----------------------------------------\n\t\t RULES\n----------------------------------------\n'\
+        '\t  Enter a number 1-9\n1 = TOP LEFT, 2 = TOP MID, 3 = TOP RIGHT'\
+        '\n4 = MID LEFT, 5 = MID MID, 6 = MID RIGHT\n7 = BOT LEFT, 8 = BOT MID, 9 = BOT RIGHT'\
+        '\n----------------------------------------\n')
 
-    print(f'{player_1.name} gets the first move! ')
+    # load empty board
+    gameboard = Game()
+    i = 1
+    player_turn = player_1
+
+    while True:
+        if i % 2 == 0:
+            player_turn = player_2
+        else:
+            player_turn = player_1
+
+        print(f'\n   Player: {player_turn.name}  |  Enter Move (1-9): \n < Type \'help\' anytime to see the move sequences. >' )
+        user_move = input()
+
+        if user_move in ['1', '2', '3', '4', '5', '6', '7', '8', '9']:
+            user_move = int(user_move)
+            if gameboard.move(user_move, player_turn):
+                print('\nCURRENT GAME BOARD\n------------------\n')
+                print(gameboard.__repr__())
+                i += 1
+            else:
+                print(f'\n\tTHAT SPOT IS ALREADY TAKEN!\n')
+        elif user_move == 'help':
+            help()
+        elif user_move == 'quit':
+            break
+        else:
+            print(f'\nINVALID COMMAND! << Need help?  Type "help" in the command line. >> \n')
+
+        if gameboard.is_game_over(player_turn) == True:
+            if gameboard.calc_winner(player_turn) == "X":
+                print(f'\n{player_turn.name} wins!\n\n')
+                print(' THANK YOU FOR PLAY PYTHON TIC-TAC-TOE')
+                print('\n----------------------------------------')
+                print(f'xx      xx      o ooo o      xx      xx\n  xx  xx      oo       oo      xx  xx\n    xx       oo         oo       xx'\
+                    '\n  xx  xx      oo       oo      xx  xx\nxx      xx      o ooo o      xx      xx')
+                print('----------------------------------------')
+                print('\u00A9 2021 Python Tic-Tac-Toe, Inc.\n')
+                break
+            elif gameboard.calc_winner(player_turn) == "O":
+                print(f'\n{player_turn.name} wins!\n\n')
+                print(' THANK YOU FOR PLAY PYTHON TIC-TAC-TOE')
+                print('\n----------------------------------------')
+                print(f'xx      xx      o ooo o      xx      xx\n  xx  xx      oo       oo      xx  xx\n    xx       oo         oo       xx'\
+                    '\n  xx  xx      oo       oo      xx  xx\nxx      xx      o ooo o      xx      xx')                
+                print('----------------------------------------')
+                print('\u00A9 2021 Python Tic-Tac-Toe, Inc.\n')
+                break
+            else:
+                print('\nTie Game!\n\n')
+                print(' THANK YOU FOR PLAY PYTHON TIC-TAC-TOE')
+                print('\n----------------------------------------\n')
+                print(f'xx      xx      o ooo o      xx      xx\n  xx  xx      oo       oo      xx  xx\n    xx       oo         oo       xx'\
+                    '\n  xx  xx      oo       oo      xx  xx\nxx      xx      o ooo o      xx      xx')                
+                print('----------------------------------------')
+                print('\n\u00A9 2021 Python Tic-Tac-Toe, Inc.')
+                break
 
 main()
