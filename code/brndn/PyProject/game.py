573b120659ad3e7033ab808d1860b81aa9f7243f
Brandon Rimes <Brandon.Rimes@yahoo.com>
2021-02-16 13:24:12 -0500

    getting somewhere

--- code/brndn/PyProject/game.py
+++ code/brndn/PyProject/game.py
@@ -18,10 +18,13 @@ pg.mouse.set_visible(False)
 #mx, my = pg.mouse.get_pos()
 bkgrnd = pg.transform.scale(pg.image.load('matrixcode.jpg'),(1000,600))
 
-def draw_window(player,enemy):
+
+def draw_window(player,enemy,enemies):
     window.blit(bkgrnd,(0,0))
     window.blit(donut,(player.x,player.y))
-    window.blit(manatee,(enemy.x,enemy.y))
+    for enemy in enemies:
+        window.blit(manatee,(enemy.x,enemy.y))
+
     pg.display.update()
     
 
@@ -29,8 +32,10 @@ def draw_window(player,enemy):
 def main():
     #mx, my = pg.mouse.get_pos()
     player = pg.Rect(500,200,70,70)
-    enemy = pg.Rect(0,0,70,70)
+    #enemy = pg.Rect(0,0,70,70)
+    enemies = [pg.Rect(0,0,70,70)]
     step_size = 4
+    loopcount = 1
     clock = pg.time.Clock()
 
     run = True
@@ -45,17 +50,27 @@ def main():
             player.x = mx-45
             player.y = my-45
 
-        player_vector = pg.math.Vector2(*(player.x,player.y))
-        enemy_vector = pg.math.Vector2(*(enemy.x,enemy.y))
-        new_enemy_vector = pg.math.Vector2(*(enemy.x,enemy.y))
-        distance = enemy_vector.distance_to(player_vector)
-        if distance > 0:
-            direction_vector = (player_vector - enemy_vector) / distance
-            step_distance = min(distance, step_size)
-            new_enemy_vector = enemy_vector + direction_vector * step_distance
-            enemy.x,enemy.y = new_enemy_vector
-            
-        draw_window(player,enemy)
+        for enemy in enemies:
+            player_vector = pg.math.Vector2(*(player.x,player.y))
+            enemy_vector = pg.math.Vector2(*(enemy.x,enemy.y))
+            new_enemy_vector = pg.math.Vector2(*(enemy.x,enemy.y))
+            distance = enemy_vector.distance_to(player_vector)
+        
+            if distance > 0:
+                direction_vector = (player_vector - enemy_vector) / distance
+                step_distance = min(distance, step_size)
+                new_enemy_vector = enemy_vector + direction_vector * step_distance
+                enemy.x,enemy.y = new_enemy_vector
+
+            collide = player.colliderect(enemy)
+            if collide:
+                enemies = [pg.Rect(0,0,70,70)]
+        
+
+        if loopcount % 300 == 0:  
+            enemies.append(pg.Rect(0,0,70,70))   
+        draw_window(player,enemy,enemies)
+        loopcount += 1
 
 
 if __name__=="__main__":
