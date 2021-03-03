8bf6ed3558007f17a12363967920aea565ccfec0
Richard Farr <RichardFarr7@gmail.com>
2021-03-03 10:04:48 -0800

    fizz buzz in python

--- code/richard/django/fizz_buzz.py
+++ code/richard/django/fizz_buzz.py
@@ -8,4 +8,15 @@ For multiples of three print “Fizz” instead of the number and for the multip
 For numbers which are multiples of both three and five print “FizzBuzz”.
 '''
 
-print("test1")
+for fizzbuzz in range(1, 101):
+    if fizzbuzz % 3 == 0 and fizzbuzz % 5 == 0:
+        print("FizzBuzz")
+        continue
+    elif fizzbuzz % 3 == 0:
+        print("Fizz")
+        continue
+    elif fizzbuzz % 5 == 0:
+        print("Buzz")
+        continue
+    print(fizzbuzz)
+	
