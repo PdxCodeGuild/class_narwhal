dd98db261cf8a19d502ab6b09d8782506c9cb82c
Richard Farr <RichardFarr7@gmail.com>
2021-03-03 09:48:34 -0800

    django section folder

---
+++ code/richard/django/fizz_buzz.py
@@ -0,0 +1,11 @@
+# Richard 
+# March 3rd, 2021
+# Fizz Buzz
+
+'''
+Write a Python program which iterates the integers from 1 to 100. 
+For multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. 
+For numbers which are multiples of both three and five print “FizzBuzz”.
+'''
+
+print("test1")
