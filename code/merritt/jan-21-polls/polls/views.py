99d57e91b2db4e831d18f9b43fce137c2d6d8b68
Merritt Lawrenson <merrittlawrenson@gmail.com>
2021-03-05 09:51:07 -0800

    adding real views and templates

--- code/merritt/jan-21-polls/polls/views.py
+++ code/merritt/jan-21-polls/polls/views.py
@@ -1,4 +1,19 @@
 from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+
+from .models import Question
 
 def index(request):
-    return HttpResponse("Hello World!!!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+def results(request, question_id):
+    return HttpResponse(f"You're looking at the results of question {question_id}.")
+
+def vote(request, question_id):
+    return HttpResponse(f"You're voting on question {question_id}.")
