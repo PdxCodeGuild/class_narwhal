f6ca9c9ae1e37fe8e8a8a41683c101e69c6b06fc
Richard Farr <RichardFarr7@gmail.com>
2021-03-05 12:26:19 -0800

    poll app - progress

--- code/richard/django/polls/polls/views.py
+++ code/richard/django/polls/polls/views.py
@@ -1,4 +1,38 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+# from django.template import loader
+from django.shortcuts import render, get_object_or_404
+from django.urls import reverse
+
+from .models import Question, Choice
 
 def index(request):
-    return HttpResponse("Hello World!")
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    # template = loader.get_template('polls/index.html')
+    context = {'latest_question_list': latest_question_list}
+    # output = ', '.join([q.question_text for q in latest_question_list])
+    return render(request, 'polls/index.html', context)
+
+def detail(request, question_id):
+    # return HttpResponse(f"You're looking at question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/detail.html', {'question': question})
+
+
+def results(request, question_id):
+    # return HttpResponse(f"You're looking at the results of question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, 'polls/results.html', {'question': question})
+
+def vote(request, question_id):
+    # return HttpResponse(f"You're voting on question {question_id}.")
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except(KeyError, Choice.DoesNotExist):
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You did't select a valid choice"
+        })
+    selected_choice.votes += 1
+    selected_choice.save()
+    return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
