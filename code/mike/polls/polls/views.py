58335b626a118866c84765224f7de40663db811f
Mike Price <MrPrice77@gmail.com>
2021-03-05 16:30:17 -0800

    Added Tests and CSS

--- code/mike/polls/polls/views.py
+++ code/mike/polls/polls/views.py
@@ -1,16 +1,19 @@
+from datetime import timedelta
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, get_object_or_404
 from django.urls import reverse
+from django.utils import timezone
 
 from .models import Question, Choice
 
 def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    latest_question_list = Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
     context = {'latest_question_list': latest_question_list}
     return render(request, 'polls/index.html', context)
 
 def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
+    query_set = Question.objects.filter(pub_date__lte=timezone.now())
+    question = get_object_or_404(query_set, pk=question_id)
     return render(request, 'polls/detail.html', {'question' : question})
 
 def results(request, question_id):
