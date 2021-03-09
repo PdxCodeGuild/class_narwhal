846656f5533f4a7c1ff184c80d655fc8eff25625
Richard Farr <RichardFarr7@gmail.com>
2021-03-09 10:55:55 -0800

    django grocery list progress

--- code/richard/django/grocery_list/grocery_list/views.py
+++ code/richard/django/grocery_list/grocery_list/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Grocery_Item
+from .forms import ItemForm
 
 def grocery_list(request):
     grocery_items = Grocery_Item.objects.all()
@@ -19,3 +20,28 @@ def grocery_item_detail(request, id):
     return render(request, "grocery_list/item_detail.html", context)
 
 
+def item_create(request):
+    form = ItemForm(request.POST or None)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "grocery_list/item_create.html", context)
+
+
+def item_update(request, id):
+    grocery_item = Grocery_Item.objects.get(id=id)
+    form = ItemForm(request.POST or None, instance = grocery_item)
+    if form.is_valid():
+        print(form.cleaned_data)
+        form.save()
+        return redirect('/')
+    context = {"form": form}
+    return render(request, "grocery_list/item_update.html", context)
+
+
+def item_delete(request, id):
+    grocery_item = Grocery_Item.objects.get(id=id)
+    grocery_item.delete()
+    return redirect('/')
