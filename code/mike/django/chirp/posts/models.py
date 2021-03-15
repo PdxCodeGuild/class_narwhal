a407db8f5852841ffa13f63ac9cb10ed0316f713
Mike Price <MrPrice77@gmail.com>
2021-03-15 16:40:14 -0700

    Lab 3

---
+++ code/mike/django/chirp/posts/models.py
@@ -0,0 +1,21 @@
+from django.db import models
+from django.urls import reverse
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('posts:detail', args=(self.id,))
+
+    class Meta:
+        ordering = ['-created']
+
+class MyModel(models.Model):
+    my_image = models.ImageField(upload_to='images/')
