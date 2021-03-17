8cd6ad73e7df48975682be041a2dc8da065a34f4
Richard Farr <RichardFarr7@gmail.com>
2021-03-17 10:03:35 -0700

    django twitter - permissions added and working

--- code/richard/django/4_Chirp/posts/views.py
+++ code/richard/django/4_Chirp/posts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Post
 from django.urls import reverse_lazy
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
 # Create your views here.
 
@@ -13,17 +14,29 @@ class PostDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
 
-class PostCreateView(CreateView):
+class PostCreateView(LoginRequiredMixin, CreateView):
     model = Post
     template_name = 'post_new.html'
-    fields = ['content', 'date_posted', 'author']
+    fields = ['content', 'date_posted']
 
-class PostUpdateView(UpdateView):
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin ,UpdateView):
     model = Post
     template_name = 'post_edit.html'
     fields = ['content']
 
-class PostDeleteView(DeleteView):
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Post
     template_name = 'post_delete.html'
     success_url = reverse_lazy('posts:home')
+
+    def test_func(self):
+        post = self.get_object()
+        return self.request.user == post.author
