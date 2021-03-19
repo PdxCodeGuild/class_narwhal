07f18f0baec35d5c0766f9d27316087c6a95a36b
Charles Spahn <charlies87@gmail.com>
2021-03-19 11:34:16 -0700

    Lab1-JS_rewrite

---
+++ code/charlie/JavaScript/main.js
@@ -0,0 +1,10 @@
+//alert("hello World!!!");
+let y = 20
+console.log(y)  
+
+let my_name = prompt("What is my name?")
+alert("Hello " + my_name + ", hope you're well!")
+
+alert(`This is the number you coded:${y} and printed through a JS f-string`)//F-strings in JavaScript
+
+console.log(typeof(NaN))//make a meme of this...
