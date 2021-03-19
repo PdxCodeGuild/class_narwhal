920a681ee1803270a69afc17ae6da9c47be12fd3
Mike Price <MrPrice77@gmail.com>
2021-03-18 23:07:10 -0700

    Conversion 1 complete

--- code/mike/django/chirp/users/models.py
+++ code/mike/django/chirp/users/models.py
@@ -3,12 +3,15 @@ from django.urls import reverse
 
 class Profile(models.Model):
     user = models.OneToOneField('auth.User', on_delete=models.CASCADE, null=True, blank=True)
-    location = models.TextField(blank=True, null=True)
-    bio = models.TextField(blank=True, null=True)
-    image = models.URLField(blank=True, null=True)
+    location = models.CharField(max_length=100, blank=True, null=True)
+    bio = models.TextField(max_length=500, blank=True, null=True)
+    # avatar = models.ImageField(upload_to='images', null=True)
 
     def __str__(self):
         return self.user.username
 
     def get_absolute_url(self):
         return reverse('users:profile', args=(self.user.username,))
+
+class MyModel(models.Model):
+    my_image = models.ImageField(upload_to='images/')
