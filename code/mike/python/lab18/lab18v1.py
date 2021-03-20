30ca389d137aa863756db55630f96d0d8d4451dc
Mike Price <MrPrice77@gmail.com>
2021-03-19 21:16:57 -0700

    Add missing and incomplete labs

--- code/mike/python/lab18/lab18v1.py
+++ code/mike/python/lab18/lab18v1.py
@@ -9,26 +9,34 @@ Peaks and Valleys
 data = [1, 2, 3, 4, 5, 6, 7, 6, 5, 4, 5, 6, 7, 8, 9, 8, 7, 6, 7, 8, 9]
 
 # Generate list of peak indices
-peaks = []
-for p in range(1,len(data()-1):
-    before = data[p-1]
-    current = data[p]
-    after = data[p+1]
-    if before < current > after:
-        peaks.append(p)
-print(peaks)
+def peaks(data):
+    peaks = []
+    for p in range(1,len(data)-1):
+        before = data[p-1]
+        current = data[p]
+        after = data[p+1]
+        if before < current > after:
+            peaks.append(p)
+    return peaks
+
 
 # Generate list of valley indices
-valleys = []
-for v in range(len(data[0:-1])):
-    before = data[v-1]
-    current = data[v]
-    after = data[v+1]
-    if before > current < after:
-        valleys.append(v)
-print(valleys)
+def valleys(data):
+    valleys = []
+    for v in range(1,len(data)-1):
+        before = data[v-1]
+        current = data[v]
+        after = data[v+1]
+        if before > current < after:
+            valleys.append(v)
+    return valleys
+
 
 # Combine the two lists into one
-peaks_and_valleys = peaks + valleys
+def peaks_and_valleys(peaks, valleys):
+    return sorted(peaks(data) + valleys(data))
 
-print(peaks_and_valleys)
+print(data)
+print(peaks(data))
+print(valleys(data))
+print(peaks_and_valleys(peaks, valleys))
--- code/mike/python/lab23/lab_23v1.py
+++ code/mike/python/lab23/lab_23v1.py
@@ -1,14 +1,21 @@
-with open('contacts.csv', 'r') as file:
-    lines = file.read().split('\n')
+'''
+Lab 23
+Version 1
+Contacts List
+'''
 
-contacts = []
+with open('contacts.csv', 'r') as file: # Opens contacts.csv
+    rows = file.read() # reads file as rows
 
-for line in lines:
-    line = line.split(',')
-    contacts.append(line)
+contacts = [] # creates empty list
 
+rows = rows.split('\n') # separates rows with line break
+header = rows[0].split(',') # Identifies the top row as header
 
-
-print(lines)
+# starts list of contacts on the first row after header
+for x in range(1, len(rows)):
+    row = rows[x].split(',')
+    contact = dict(zip(header, row))
+    contacts.append(contact)
 
 print(contacts)
