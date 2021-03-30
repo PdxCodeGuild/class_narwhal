6adcc9ce5c15fceb783b0e632685eda3199e7881
Richard Farr <RichardFarr7@gmail.com>
2021-03-30 14:58:09 -0700

    js vue todo working

--- code/richard/javascript/vue/lab_11_Vue_todo/js/site.js
+++ code/richard/javascript/vue/lab_11_Vue_todo/js/site.js
@@ -2,12 +2,23 @@ var app = new Vue({
     el: '#app',
     data: {
       title: 'Vue todo app by Rich',
-      newTodo: ''
+      newTodo: '',
+      todos: []
     },
     methods: {
+      
       addTodo() {
-        console.log(this.newTodo);
+        // console.log(this.newTodo);
+        this.todos.push({
+          title: this.newTodo,
+          done: false
+        })
+        this.newTodo = "";
+      },
 
+      removeTodo(todo) {
+        const todoIndex = this.todos.indexOf(todo);
+        this.todos.splice(todoIndex, 1);
       }
     }
   })
