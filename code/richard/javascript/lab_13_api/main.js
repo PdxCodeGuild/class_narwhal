37d6268d0f281aa0453e0e7aef9f5eb2e29fe0b9
Richard Farr <RichardFarr7@gmail.com>
2021-04-01 16:32:38 -0700

    vue api progress

--- code/richard/javascript/lab_13_api/main.js
+++ code/richard/javascript/lab_13_api/main.js
@@ -1,9 +1,23 @@
 const vm = new Vue({
     el: '#app',
     data: {
-        results: {}
+        //random25: {},
+        results: {},
+        search_term: 'Jeff Bezos',
+        search_type: 'author'
     },
     methods: {
+        loadRandom25: function() {
+            axios({
+                url: "https://favqs.com/api/quotes",
+                method: "get",
+                headers: {
+                    "Authorization": `Token token="${apiKey}"`
+                },
+            }).then(response => {
+                this.results = response.data
+            })
+        },
         loadQuotes: function() {
             axios({
                 url: "https://favqs.com/api/quotes",
@@ -12,12 +26,16 @@ const vm = new Vue({
                     "Authorization": `Token token="${apiKey}"`
                 },
                 params: {
-                    filter: "steve jobs",
-                    type: "author"
+                    filter: this.search_term, // "Will Rogers" "Jeff Bezos"
+                    type: this.search_type // keyword author "tag"
                 }
             }).then(response => {
                 this.results = response.data
             })
         }
+    },
+
+    mounted: function(){
+        this.loadRandom25()
     }
 })
