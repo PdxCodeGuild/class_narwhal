9e2670b96e06cd8e20c6962e15906dccda86902d
dondco36 <dondco36@gmail.com>
2021-04-06 16:28:53 -0700

    API

--- code/conor/javascript/lab_14/crypto.js
+++ code/conor/javascript/lab_14/crypto.js
@@ -1,26 +1,49 @@
-let clock = setInterval(timeKeeper, 1000)
-
-function timeKeeper() {
-    console.log("clock")
-    let timeNow = new Date()
-    document.getElementById('clock').innerHTML = timeNow.toLocaleString('en-US', { timeZone: 'America/New_York' })
-}
 
 const vm = new Vue({
     el: '#app',
     data: {
-        results: {"BTC": {"USD":20.00,"EUR":30.50}, 
-                  "ETH": {"USD":5.00,"EUR":6.50}} 
-     }
+        results: [],
+        topTenCrypto: [],
+        NYSETime: "",
+        HomeTime: ""
+    },
+    methods: {
+        loadCrypto: function () {
+            axios({
+                url: "https://min-api.cryptocompare.com/data/price?api_key=",
+                method: "get",
+                params: {
+                    tryConversion: true,
+                    fsym: "BTC",
+                    tsyms: "USD,JPY,EUR"
+                }
+            }).then(response => {
+                this.results = response.data
+            })
+        },
+        topTen: function () {
+            axios({
+                url: "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&api_key=",
+                method: "get",
+                params: {
+                    tsym: "USD"
+                }
+            }).then(response => {
+                this.topTenCrypto = response.data.Data
+            })
+        }
+    },
+    mounted: function () {
+        this.loadCrypto()
+        this.topTen()
+
+        const timeKeeper = ()=> {
+            
+            console.log("clock")
+            let timeNow = new Date()
+            this.NYSETime = timeNow.toLocaleString('en-US', { timeZone: 'America/New_York' })
+            this.HomeTime = timeNow.toLocaleString()
+        }
+        let clock = setInterval(timeKeeper, 1000)
+    }
 })
-    // methods: {
-    //     loadCrypto: function() {
-    //         axios({
-    //             url: "https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR",
-    //             method: "get",
-    //             .then(response => {
-    //                 this.results = response.data
-    //             })
-    //         })
-    //     }
-    // }
