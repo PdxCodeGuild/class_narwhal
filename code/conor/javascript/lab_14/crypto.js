f5e146b73ed2ab27849ec34b27b73f7774a44710
dondco36 <dondco36@gmail.com>
2021-04-07 16:31:30 -0700

    Oofta

--- code/conor/javascript/lab_14/crypto.js
+++ code/conor/javascript/lab_14/crypto.js
@@ -1,29 +1,52 @@
+Vue.component('coin-me', {
+    data: function() {
+        return {
+            filter: ""
+        }
+    },
+    template: `
+        <div>
+            <div>
+                <h3>Which Crypto Coin would you like to Search?</h3>
+                <input type="type" placeholder="Enter Search Text Here" @keyup.enter="getCrypto" v-model="filter">
+                    <button @click="getCrypto">Search</button>
+            </div>
+        </div>
+        `,
+    methods: {
+        getCrypto: function() {
+            this.$emit('crypto', this.filter)
+            this.filter = ""
+        }
+    }
+})
 
 const vm = new Vue({
     el: '#app',
     data: {
-        results: [],
+        results: {},
         topTenCrypto: [],
         NYSETime: "",
-        HomeTime: ""
+        HomeTime: "",
+        displayCoin: ""
     },
     methods: {
-        loadCrypto: function () {
-            axios({
-                url: "https://min-api.cryptocompare.com/data/price?api_key=",
-                method: "get",
-                params: {
-                    tryConversion: true,
-                    fsym: "BTC",
-                    tsyms: "USD,JPY,EUR"
-                }
-            }).then(response => {
-                this.results = response.data
-            })
-        },
+        // loadCrypto: function () {
+        //     axios({
+        //         url: "https://min-api.cryptocompare.com/data/price?",
+        //         method: "get",
+        //         params: {
+        //             tryConversion: true,
+        //             fsym: "BTC",
+        //             tsyms: "USD,JPY,EUR"
+        //         }
+        //     }).then(response => {
+        //         this.results = response.data
+        //     })
+        // },
         topTen: function () {
             axios({
-                url: "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10&api_key=",
+                url: "https://min-api.cryptocompare.com/data/top/totalvolfull?limit=10",
                 method: "get",
                 params: {
                     tsym: "USD"
@@ -31,10 +54,27 @@ const vm = new Vue({
             }).then(response => {
                 this.topTenCrypto = response.data.Data
             })
+        },
+        searchCoin: function (coinFilter) {
+            // console.log(coinFilter)
+            this.displayCoin = coinFilter
+            axios({
+                url: "https://min-api.cryptocompare.com/data/pricemultifull?",
+                method: "get",
+                params: {
+                    tryConversion: true,
+                    fsyms: coinFilter,
+                    tsyms: "USD,JPY,EUR"
+                }
+            }).then(response => {
+                this.results = response.data.DISPLAY[coinFilter]
+                // console.log(response.data)
+                // console.log(response.data.DISPLAY)
+            })
         }
     },
     mounted: function () {
-        this.loadCrypto()
+        // this.loadCrypto()
         this.topTen()
 
         const timeKeeper = ()=> {
