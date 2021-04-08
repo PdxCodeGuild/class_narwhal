7ecdb06f335f77276c0a2beea9c04ff5d6db50f2
Richard Farr <RichardFarr7@gmail.com>
2021-04-08 16:31:04 -0700

    django rest framework started

---
+++ code/richard/django_rest/apis/serializers.py
@@ -0,0 +1,13 @@
+# apis/serializers.py
+from rest_framework import serializers
+from todos import models
+
+
+class TodoSerializer(serializers.ModelSerializer):
+    class Meta:
+        fields = (
+            'id',
+            'title',
+            'description',
+        )
+        model = models.Todo
