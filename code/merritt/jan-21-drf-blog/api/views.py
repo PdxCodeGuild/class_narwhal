5f41e2a172b14b69c525927f1058a0ff49a155c4
Merritt Lawrenson <merrittlawrenson@gmail.com>
2021-04-08 14:43:17 -0700

    permissions and nested serializers

--- code/merritt/jan-21-drf-blog/api/views.py
+++ code/merritt/jan-21-drf-blog/api/views.py
@@ -1,12 +1,24 @@
-from rest_framework import generics
+from rest_framework import viewsets
+# from rest_framework import generics
+from django.contrib.auth.models import User
 
 from posts.models import Post
-from .serializers import PostSerializer
+from .serializers import PostSerializer, UserSerializer
+from .permissions import IsAuthorOrReadOnly
 
-class PostList(generics.ListCreateAPIView):
+class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all()
     serializer_class = PostSerializer
+    permission_classes = [IsAuthorOrReadOnly]
 
-class PostDetail(generics.RetrieveUpdateDestroyAPIView):
-    queryset = Post.objects.all()
-    serializer_class = PostSerializer
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# class PostList(generics.ListCreateAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
+
+# class PostDetail(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Post.objects.all()
+#     serializer_class = PostSerializer
