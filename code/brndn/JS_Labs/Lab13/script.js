6ae99501c4ea60a0a6e5dc8c5688cb8c11b8b4a3
Brandon Rimes <Brandon.Rimes@yahoo.com>
2021-05-10 20:34:38 -0400

    JS Lab13 Complete

--- code/brndn/JS_Labs/Lab13/script.js
+++ code/brndn/JS_Labs/Lab13/script.js
@@ -1,98 +1,78 @@
 Vue.component('quote-item', {
-  data: function() {
-    return {
-      text: '',
+    data: function() {
+        return {
+            text: '',
+        }
+    },
+    props: ['quote'],
+    template: `<div class="results">
+                  <h2>{{ quote.body }}</h2>
+                  <h4>-<a :href="quote.url">{{ quote.author }}</a></h4>
+              </div>`,
+    methods: {
+        searchFor: function() {
+          this.$emit('add', { text: this.text, id: this.id})
+          this.text = ''
+        }
     }
-  },
-  props: ['quote'],
-  template: `<div class="results">
-              <h2>{{ quote.body }}</h2>
-              <h4>-<a :href="quote.url">{{ quote.author }}</a></h4>
-            </div>`,
-  methods: {
-    searchFor: function() {
-      this.$emit('add', { text: this.text, id: this.id})
-      this.text = ''
-    }
-  }
 })
 
 const vm = new Vue({
-  el: '#app',
-  data: {
-      searchBy: '',
-      results: {},
-      type: '',
-  },
-  methods: {
-      randomQuotes: function() {
-          axios({
-              url: "https://favqs.com/api/quotes",
-              method: "get",
-              headers: {
-                  "Authorization": `Token token="939f9918054091444999a7f6b165633d"`
-              },
-          }).then(response => {
-            this.results = response.data
+    el: '#app',
+    data: {
+        searchBy: '',
+        results: {},
+        type: '',
+        page: '1',
+        show: false,
+    },
+    methods: {
+        randomQuotes: function() {
+            axios({
+                url: "https://favqs.com/api/quotes",
+                method: "get",
+                headers: {
+                    "Authorization": `Token token="939f9918054091444999a7f6b165633d"`
+                },
+            }).then(response => {
+                this.results = response.data
+                this.show = false
             })
-      },
-      loadQuotes: function() {
-          axios({
-              url: "https://favqs.com/api/quotes",
-              method: "get",
-              headers: {
-                  "Authorization": `Token token="939f9918054091444999a7f6b165633d"`
-              },
-              params: {
-                  filter: this.searchBy,
-                  type: this.type,
-              }
-          }).then(response => {
-              this.results = response.data
+        },
+        loadQuotes: function() {
+            axios({
+                url: "https://favqs.com/api/quotes",
+                method: "get",
+                headers: {
+                    "Authorization": `Token token="939f9918054091444999a7f6b165633d"`
+                },
+                params: {
+                    filter: this.searchBy,
+                    type: this.type,
+                    page: this.page,
+                }
+            }).then(response => {
+                  this.results = response.data
+                  this.show = true
+                  console.log(this.results)
             })
-      },
-  },
-  mounted: function() {
-    this.randomQuotes()
-  }
+        },
+        pageDown: function() {
+            var newPage = Number(this.page)
+            if (newPage > 1) {
+                newPage -= 1
+                this.page = String(newPage)
+                this.loadQuotes()
+            } 
+        },
+        pageUp: function() {
+            var newPage = Number(this.page)
+            newPage += 1
+            this.page = String(newPage)
+            this.loadQuotes()
+        },
+    },
+    mounted: function() {
+        this.randomQuotes()
+    }
 })
-
-
-// const axios = require('axios').default;
-// Vue.component('quote-item', {
-//     },
-//     props: ['quote'],
-//     template: '<li>{{ quote.text }}</li>'
-//     }),
-// const vm = new Vue({
-//     el: '#app',
-//     data: {
-//         results: {},
-//         searchBy: '',
-//     },
-//     mounted () {
-//         axios
-//         .get('https://favqs.com/api/quotes')
-//         .header(`Authorization: Token token="939f9918054091444999a7f6b165633d"`)
-//         .then(response => {
-//             this.results = response.data
-//         })
-//     },
-    // methods: {
-    //     loadQuotes: function() {
-    //         axios({
-    //             url: "https://favqs.com/api/quotes",
-    //             method: "get",
-    //             headers: {
-    //                 "Authorization": `Token token="939f9918054091444999a7f6b165633d"`
-    //             },
-    //             params: {
-    //                 filter: this.searchBy,
-    //                 type: "author"
-    //             }
-    //         }).then(response => {
-    //             this.results = response.data
-    //         })
-    //     }
-    // }
-// })
